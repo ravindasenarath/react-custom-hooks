6299d48380e328e8e264964bdde67f3886752ed4
Ravinda Senarath <ravinda.senarath@cognite.com>
2022-09-06 00:17:32 +0530

    Common login in component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useToggle } from './useToggle';
 
-function App() {
+const App = () => {
+  const [toggled, setToggled] = useState(false);
+
+  const handleOnClick = () => {
+    setToggled(toggled => !toggled);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleOnClick}>{toggled ? 'Toggled' : 'Click to Toggle'}</button>
       </header>
     </div>
   );
